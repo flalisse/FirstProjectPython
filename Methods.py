a0c07d4025a028023df1f5307beef812975da0c5
Francois <francois@MacBook-Pro-de-Sandrine.local>
2019-05-22 15:41:09 +0200

    First commit on my first project on github

---
+++ Methods.py
@@ -0,0 +1,82 @@
+'define all the method for the project'
+
+
+
+class Personnage:
+
+    def __init__(self,race,caracteristiques):
+
+
+        self._race= race
+        self._caracteristiques = caracteristiques
+
+
+    def __setteurrace(self,newrace):
+
+        self._race = newrace
+
+    def __getteurrace(self,race):
+
+        return self._race
+
+    def __setteurcaract(self,**NewCaracteristiques):
+
+        self._caracteristiques = NewCaracteristiques
+
+    def __getteurcaract(self,caracteristiques):
+
+        return self._caracteristiques
+
+
+    def __str__(self):
+
+        return "Voici les caractéristiques de votre personnage  race : {} Caracteristiques: {}".format(self.race,self.caracteristiques)
+
+
+
+
+
+    race = property(__getteurrace,__setteurrace)
+
+
+
+
+def choixrace():
+
+    newrace=input("veuillez sélectionner votre race entre : Humain / Orc")
+
+    while newrace != "Orc" and newrace != "Humain":
+
+        return choixrace()
+
+    Personnage.race = newrace
+
+
+
+def setupcaracteristiques():
+
+    if Personnage.race == "Orc":
+        NewCaracteristiques = {
+
+            "Armor":5,
+            "Defense":10
+        }
+
+    elif Personnage.race =="Humain":
+
+        NewCaracteristiques = {
+
+
+            "Armor":7,
+            "Defense":8
+        }
+
+    Personnage.caracteristiques = NewCaracteristiques
+
+
+
+
+
+
+
+
---
+++ RunningGame.py
@@ -0,0 +1,15 @@
+from Methods import choixrace
+
+from Methods import Personnage,setupcaracteristiques
+
+print("first of all select your race: ")
+
+RaceSelect = choixrace()
+
+Carac = setupcaracteristiques()
+
+
+YourPerso = Personnage(RaceSelect,Carac)
+
+print(YourPerso)
+
